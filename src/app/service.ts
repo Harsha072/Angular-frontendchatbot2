b4254dce57631bd3f7e651f71ae80fe5c7b46a45
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-28 23:30:41 +0100

    check

--- src/app/service.ts
+++ src/app/service.ts
@@ -68,6 +68,7 @@ export class DataService {
   login(user: any): Observable<any> {
     console.log("calling login",user)
     return this.http.post<any>(`${environment.apiUrl}/harsha`, user)
+    
   }
   logout(user: any): Observable<any> {
     console.log("calling logout")
