507b2ee6e8779e766f91c1dbb551f07187f18de1
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-28 17:02:49 +0100

    change4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://9tbppxc74a.execute-api.us-east-1.amazonaws.com/dev/api'
+    apiUrl: 'https://9tbppxc74a.execute-api.us-east-1.amazonaws.com/dev/api/'
     // apiUrl:'http://localhost:8080/api'
   }
