7eb82faaa09e10274a296257691a45e09990dc5b
20100308@mail.wit.ie <20100308@mail.wit.ie>
2023-04-28 20:03:01 +0100

    magic

--- src/app/service.ts
+++ src/app/service.ts
@@ -68,7 +68,6 @@ export class DataService {
   login(user: any): Observable<any> {
     console.log("calling login",user)
     return this.http.post<any>(`https://9tbppxc74a.execute-api.us-east-1.amazonaws.com/dev/api/harsha`, user)
-
   }
   logout(user: any): Observable<any> {
     console.log("calling logout")
